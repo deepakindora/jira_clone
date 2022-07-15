27b4de0b2f3b3599cd8ae4ab6d780bb3cd74c9af
Indora <deepak.indora@capgemini.com>
2022-07-15 15:19:35 +0530

    Updates

---
+++ precache-manifest.3958bbf2085f72820ee71116bc2b8970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28401872cce20ef86f3",
+    "url": "/jira_clone/static/css/main.531a0b6b.chunk.css"
+  },
+  {
+    "revision": "a28401872cce20ef86f3",
+    "url": "/jira_clone/static/js/main.a2840187.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/jira_clone/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "c17f31af081b8aaeffe2",
+    "url": "/jira_clone/static/css/2.d81544c8.chunk.css"
+  },
+  {
+    "revision": "c17f31af081b8aaeffe2",
+    "url": "/jira_clone/static/js/2.c17f31af.chunk.js"
+  },
+  {
+    "revision": "5b88aab2f41e7fb2796d",
+    "url": "/jira_clone/static/js/runtime~main.5b88aab2.js"
+  },
+  {
+    "revision": "4de46a25417a5bed12e146bcc6e69ab1",
+    "url": "/jira_clone/index.html"
+  }
+];
